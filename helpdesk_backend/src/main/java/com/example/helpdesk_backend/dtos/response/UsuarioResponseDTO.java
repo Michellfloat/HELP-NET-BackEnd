2b4a346e2456f11f5563eea7f89b7ffd160d82f4
michellfloat <michell.ssfloat@gmail.com>
2026-08-19 20:23:27 -0300

    feat:Subindo todos os DTOs do sistema seguindo os requisitos e regras de negócio

---
+++ helpdesk_backend/src/main/java/com/example/helpdesk_backend/dtos/response/UsuarioResponseDTO.java
@@ -0,0 +1,18 @@
+package com.example.helpdesk_backend.dtos.response;
+
+import com.example.helpdesk_backend.model.enums.NivelAntendente;
+import com.example.helpdesk_backend.model.enums.Perfil;
+
+//DTO de Resposta (Nunca retorna a senha)
+public record UsuarioResponseDTO(
+    Long id,
+    String nome,
+    String email,
+    String cargo,
+    String setor,
+    Perfil perfil,
+    NivelAntendente nivelAntendente,
+    Boolean cadastroCompleto
+) {
+
+}
