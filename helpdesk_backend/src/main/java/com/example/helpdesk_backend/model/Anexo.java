33da73d3d7894ed2083cbaf6e5aaa45ba5e16791
michellfloat <michell.ssfloat@gmail.com>
2026-08-20 14:41:58 -0300

    feat:Adicionando a entidade de Anexo de Arquivos no chamado

---
+++ helpdesk_backend/src/main/java/com/example/helpdesk_backend/model/Anexo.java
@@ -0,0 +1,51 @@
+package com.example.helpdesk_backend.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "tab_anexos")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Anexo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nomeArquivo;
+
+    @Column(nullable = false)
+    private String tipoArquivo;
+
+    @Column(nullable = false)
+    private Long tamanho;
+
+    @Column(nullable = false)
+    private String caminhoArquivo;
+
+    @Column(nullable = false)
+    private LocalDateTime dataUpload;
+
+    @ManyToOne
+    @JoinColumn(name = "chamado_id", nullable = false)
+    private Chamado chamado;
+
+    @ManyToOne
+    @JoinColumn
+    private Usuario enviadoPor;
+}
