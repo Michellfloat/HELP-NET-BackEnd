0623191e040207334b060ec4f620e237a6232ecd
michellfloat <michell.ssfloat@gmail.com>
2026-08-25 16:21:54 -0300

    Subindo uma pasta contendo os TODOs(afazeres) restantes do BackEnd

--- helpdesk_backend/src/main/java/com/example/helpdesk_backend/security/JwtAuthenticationFilter.java
+++ helpdesk_backend/src/main/java/com/example/helpdesk_backend/security/JwtAuthenticationFilter.java
@@ -63,4 +63,5 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
         }
         return authHeader.substring(7); // Remove "Bearer " (com o espaço) corretamente
     }
+    //TODO: Resolver problemas de criação de tokens diferentes(Ex:Criar o token de Admin, criar o token de Atendente e criar o token de usuário diferentes um do outro, com restrições como:Admin acessa tudo, Atendente só pode acessar algumas áreas determinadas pelo nível, e usuário só tem direito as questões envolvendo seus chamados)
 }
