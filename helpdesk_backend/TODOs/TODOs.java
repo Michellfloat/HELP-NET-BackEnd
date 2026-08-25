6b6c1f246a57d5580d0606379be8933454f81623
michellfloat <michell.ssfloat@gmail.com>
2026-08-25 16:23:11 -0300

    Subindo uma pasta contendo os TODOs(afazeres) restantes do BackEnd

--- helpdesk_backend/TODOs/TODOs.java
+++ helpdesk_backend/TODOs/TODOs.java
@@ -1,5 +1,10 @@
+
+
 //TODO: Resolver problemas de criação de tokens diferentes(Ex:Criar o token de Admin, criar o token de Atendente e criar o token de usuário diferentes um do outro, com restrições como:Admin acessa tudo, Atendente só pode acessar algumas áreas determinadas pelo nível, e usuário só tem direito as questões envolvendo seus chamados)
+
+
+
 
 //TODO: Criar um Enum SETOR e atribuir ao projeto completo;
 
-//TODO: 
+//TODO: Configurar o CORSConfig
