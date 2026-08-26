e6e6beef4133e55a703dff64c894c55da7f3d8d4
michellfloat <michell.ssfloat@gmail.com>
2026-08-26 10:20:56 -0300

    fix(DataInitializer):Consertando erro da criação base do usuário Admin

--- helpdesk_backend/TODOs/TODOs.java
+++ helpdesk_backend/TODOs/TODOs.java
@@ -3,8 +3,3 @@
 //TODO: Resolver problemas de criação de tokens diferentes(Ex:Criar o token de Admin, criar o token de Atendente e criar o token de usuário diferentes um do outro, com restrições como:Admin acessa tudo, Atendente só pode acessar algumas áreas determinadas pelo nível, e usuário só tem direito as questões envolvendo seus chamados)
 
 
-
-
-//TODO: Criar um Enum SETOR e atribuir ao projeto completo;
-
-//TODO: Configurar o CORSConfig
