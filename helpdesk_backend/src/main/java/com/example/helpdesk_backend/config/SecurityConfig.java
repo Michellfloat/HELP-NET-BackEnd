25a6a1fac37e306228b47386aa07f9c178573678
michellfloat <michell.ssfloat@gmail.com>
2026-08-28 19:32:28 -0300

    Consertando erro da '/' no SecurityConfig

--- helpdesk_backend/src/main/java/com/example/helpdesk_backend/config/SecurityConfig.java
+++ helpdesk_backend/src/main/java/com/example/helpdesk_backend/config/SecurityConfig.java
@@ -62,9 +62,9 @@ public class SecurityConfig {
                     .requestMatchers(HttpMethod.POST, "/chamados").authenticated()
                     .requestMatchers(HttpMethod.GET, "/chamados").authenticated()
                     .requestMatchers(HttpMethod.POST, "/chamados/*/escalonar").hasAnyRole("ADMIN", "ATENDENTE")
-                    .requestMatchers(HttpMethod.PATCH, "chamados/*/assumir").hasAnyRole("ADMIN", "ATENDENTE")
-                    .requestMatchers(HttpMethod.PATCH, "chamados/*/status").hasAnyRole("ADMIN", "ATENDENTE")
-                    .requestMatchers(HttpMethod.PATCH, "chamados/*/avaliar").authenticated()
+                    .requestMatchers(HttpMethod.PATCH, "/chamados/*/assumir").hasAnyRole("ADMIN", "ATENDENTE")
+                    .requestMatchers(HttpMethod.PATCH, "/chamados/*/status").hasAnyRole("ADMIN", "ATENDENTE")
+                    .requestMatchers(HttpMethod.PATCH, "/chamados/*/avaliar").authenticated()
 
                     // Gestão de Anexos
                     .requestMatchers(HttpMethod.POST, "/chamados/*/anexos").authenticated()
